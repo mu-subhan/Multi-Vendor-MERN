bad9581a8420d1239e63c970e54c7c50c38dcc0a
Muhammad Subhan <muhammadsubhan8934@gmail.com>
2024-11-02 00:17:18 +0500

    setup for frontend and backend has done

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App(){
+  return(
+    <>
+   <h1 className="text-3xl text-blue-600 underline">
+      set up is ready 
+    </h1>
+  
+    </>
+  )
+}
+export default App;
