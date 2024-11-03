131f0b0a8dcdc68135cf98c85441f4ec4a08d99a
Muhammad Subhan <muhammadsubhan8934@gmail.com>
2024-11-03 22:56:01 +0500

    setup is ready for sign up

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import {BrowserRouter,Routes,Route} from "react-router-dom";
-import {LoginPage} from './Routes.js';
+import {LoginPage,SignupPage} from './Routes.js';
 
 function App(){
   return(
 <BrowserRouter>
 <Routes>
   <Route path="/login" element = {<LoginPage />}/>
+  <Route path="/sign-up" element = {<SignupPage />} />
 </Routes>
 
 </BrowserRouter>
