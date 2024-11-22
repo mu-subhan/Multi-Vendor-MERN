b72adb012a98d1ab09c2e90e21f066420c2d0283
Muhammad Subhan <muhammadsubhan8934@gmail.com>
2024-11-22 13:02:13 +0500

    complete best deal components

--- frontend/src/components/Route/productCard/productCard.jsx
+++ frontend/src/components/Route/productCard/productCard.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import styles from "../../../styles/styles"
-import { AiFillStar, AiOutlineStar } from 'react-icons/ai';
+import { AiFillHeart, AiFillStar, AiOutlineEye, AiOutlineHeart, AiOutlineShoppingCart, AiOutlineStar } from 'react-icons/ai';
 
 
 const productCard = ({data}) => {
@@ -40,7 +40,49 @@ const productCard = ({data}) => {
 
 </div>
 
-   </Link>
+  <div className='py-2 flex items-center justify-between'>
+    <div className='flex'>
+      <h5 className={`${styles.productDiscountPrice}`}>
+        {data.price === 0 ? data.price : data.discount_price}
+        $
+      </h5>
+      <h4 className={`${styles.price}`}>
+        {data.price ? data.price + " $" : null}
+      </h4>
+    </div>
+    <span className='font-[400] text-[17px] text-[#68d284]'>
+      {data.total_sell} sold
+    </span>
+  </div>
+  </Link>
+   {/* side opetion  */}
+  <div>
+    { click ? (
+    <AiFillHeart size={22}
+     className='cursor-pointer absolute right-2 top-5' 
+    onClick={() => setClick(!click)}
+    color={click ? "red" : "#333"}
+    title='Remove from wishlist'/>
+    ) : (
+      <AiOutlineHeart size={22}
+      className='cursor-pointer absolute right-2 top-5' 
+     onClick={() => setClick(!click)}
+     color={click ? "red" : "#333"}
+     title='Add to wishlist'/>
+    )}
+     <AiOutlineEye size={22}
+     className='cursor-pointer absolute right-2 top-14' 
+    onClick={() => setOpen(!open)}
+    color="#333"
+    title='Quick View'/>
+
+<AiOutlineShoppingCart size={22}
+     className='cursor-pointer absolute right-2 top-24' 
+    onClick={() => setOpen(!open)}
+    color="#444"
+    title='Add to cart'/>
+  </div>
+   
     </div>
     </>
   )
