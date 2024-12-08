94d14a7966b2ef4689ba5397245560a6a4e7b4c5
Muhammad Subhan <muhammadsubhan8934@gmail.com>
2024-12-08 23:26:05 +0500

    add some state in paymentpage

--- frontend/src/pages/PaymentPage.jsx
+++ frontend/src/pages/PaymentPage.jsx
@@ -1,6 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 
 const PaymentPage = () => {
+  const [orderData,setOrderData] = useState([]);
+  const [open,setOpen] = useState(false);
+  const {user} = useSelector((state)=> state.user);
+  const navigate = useNavigate();
+
   return (
     <div>
       
