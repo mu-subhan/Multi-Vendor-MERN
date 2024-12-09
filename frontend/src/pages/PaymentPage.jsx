c7df263fea613b59107a45397affa80a1e968ef4
Muhammad Subhan <muhammadsubhan8934@gmail.com>
2024-12-09 23:33:03 +0500

    seond part of payement is done

--- frontend/src/pages/PaymentPage.jsx
+++ frontend/src/pages/PaymentPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 
@@ -8,6 +8,33 @@ const PaymentPage = () => {
   const {user} = useSelector((state)=> state.user);
   const navigate = useNavigate();
 
+
+  useEffect(()=>{
+    const orderData = JSON.parse(localStorage.getItem("latestOrder"));
+    setOrderData(orderData);
+  },[]);
+
+  const createOrder = (data,action)=>{
+    return action.order.create({
+      purchase_units:[
+        {
+          description:"Sun-flower",
+          amount:{
+            currency_code:"USD",
+            value:orderData?.totalPrice,
+          },
+        },
+      ],
+      // not needed if a shipping address is actually needed
+      application_context:{
+        shipping_preference:"NO_SHIPPING",
+      }
+    })
+    .THEN((orderID)=>{
+      return orderID;
+    });
+  };
+  
   return (
     <div>
       
