84108062044c8c17e63c7afca385c5427c6eef89
Muhammad Subhan <muhammadsubhan8934@gmail.com>
2024-12-15 19:52:27 +0500

    function of handling payment

--- frontend/src/components/Payment/Payment.jsx
+++ frontend/src/components/Payment/Payment.jsx
@@ -1,45 +1,167 @@
-import React, { useEffect, useState } from 'react'
-import { useSelector } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
+import axios from "axios";
+import { application } from "express";
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { server } from "../../server";
+import { toast } from "react-toastify";
 
 const Payment = () => {
-  const [orderData,setOrderData] = useState([]);
-  const [open,setOpen] = useState(false);
-  const {user} = useSelector((state)=> state.user);
+  const [orderData, setOrderData] = useState([]);
+  const [open, setOpen] = useState(false);
+  const { user } = useSelector((state) => state.user);
   const navigate = useNavigate();
+  const stripe = useStripe();
+  const element = useElement();
 
-
-  useEffect(()=>{
+  useEffect(() => {
     const orderData = JSON.parse(localStorage.getItem("latestOrder"));
     setOrderData(orderData);
-  },[]);
+  }, []);
 
-  const createOrder = (data,action)=>{
-    return action.order.create({
-      purchase_units:[
-        {
-          description:"Sun-flower",
-          amount:{
-            currency_code:"USD",
-            value:orderData?.totalPrice,
+  const createOrder = (data, action) => {
+    return action.order
+      .create({
+        purchase_units: [
+          {
+            description: "Sun-flower",
+            amount: {
+              currency_code: "USD",
+              value: orderData?.totalPrice,
+            },
           },
+        ],
+        // not needed if a shipping address is actually needed
+        application_context: {
+          shipping_preference: "NO_SHIPPING",
         },
-      ],
-      // not needed if a shipping address is actually needed
-      application_context:{
-        shipping_preference:"NO_SHIPPING",
+      })
+      .then((orderID) => {
+        return orderID;
+      });
+  };
+
+  const order = {
+    cart: orderData?.cart,
+    shippingAddress: orderData?.shippingAddress,
+    user: user && user,
+    totalPrice: orderData?.totalPrice,
+  };
+
+  const onApprove = async (data, action) => {
+    return action.order.capture().then(function (details) {
+      const { payer } = details;
+
+      let paymentInfo = payer;
+      if (paymentInfo !== undefined) {
+        paypalPaymentHandler(paymentInfo);
       }
-    })
-    .THEN((orderID)=>{
-      return orderID;
     });
   };
-  
-  return (
-    <div>
-      
-    </div>
-  )
-}
 
-export default Payment
+  const paypalPaymentHandler = async (paymentInfo) => {
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+    order.paymentInfo = {
+      id: paymentInfo.payer_id,
+      status: "succeeded",
+      type: "Paypal",
+    };
+
+    await axios
+      .post(`${server}/order/create-order`, order, config)
+      .then((res) => {
+        setOpen(false);
+        navigate("order/success");
+        toast.success(" Order Successfully");
+        localStorage.setItem(cartItems, JSON.stringify([]));
+        localStorage.setItem("latestOrder", JSON.stringify([]));
+        window.location.reload();
+      });
+  };
+  const paymentData = {
+    amount: Math.round(orderData?.totalPrice * 100),
+  };
+
+  const paymentHandler = async (e) => {
+    e.preventDefault();
+    try {
+      const config = {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      };
+      const { data } = await axios.post(
+        `${server}/payment/process`,
+        paymentData,
+        config
+      );
+      const client_secret = data.client_secret;
+
+      if (!stripe || !element) return;
+      const result = await stripe.confirmCardPayment(client_secret, {
+        payment_method: {
+          card: element.getElement(CardNumberElement),
+        },
+      });
+
+      if (result.error) {
+        toast.error(result.error.message);
+      } else {
+        if (result.paymentIntent.status === "succeeded") {
+          order.paymentInfo = {
+            id: result.paymentIntent.id,
+            status: result.paymentIntent.status,
+            type: "CCredit Card",
+          };
+
+          await axios
+            .post(`${server}/order/create-order`.order, config)
+            .then((res) => {
+              setOpen(false);
+              navigate("/order/success");
+              toast.success("Order successful!");
+              localStorage.setItem("cartItems", JSON.stringify([]));
+              localStorage.setItem("latestOrder", JSON.stringify([]));
+              window.location.reload();
+            });
+        }
+      }
+    } catch (error) {
+        toast.error(error);
+    }
+  };
+
+  const cashOneDeliveryHandler = async (e) =>{
+    e.preventDefault();
+
+    const config= {
+        headers:{
+            "Content-Type":"application/json",
+        }
+    }
+    order.paymentInfo={
+        type:"Cash On Delivery",
+    };
+
+    await axios.post(`${server}/order/create-order`,order.config)
+    .then((res)=>{
+        setOpen(false)
+        navigate("/order/success");
+        toast.success("Order successful!");
+              localStorage.setItem("cartItems", JSON.stringify([]));
+              localStorage.setItem("latestOrder", JSON.stringify([]));
+              window.location.reload();
+    })
+  }
+
+  return( 
+  <div>
+
+  </div>)
+};
+
+export default Payment;
