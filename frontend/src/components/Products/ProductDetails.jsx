9a967d8bfb8ae47d4321f380fddd130e95b1e664
Muhammad Subhan <muhammadsubhan8934@gmail.com>
2024-12-28 21:56:05 +0500

    done with other part

--- frontend/src/components/Products/ProductDetails.jsx
+++ frontend/src/components/Products/ProductDetails.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react'
-import { useNavigate } from 'react-router-dom';
+import React, { act, useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom';
 import styles from '../../styles/styles';
 import { AiFillHeart, AiOutlineHeart, AiOutlineMessage, AiOutlineShoppingCart } from 'react-icons/ai';
 
@@ -121,7 +121,9 @@ const ProductDetails = ({data}) => {
 
                     </div>
                 </div>
-                
+                <ProductDetailsInfo data ={data}/>
+                <br />
+                <br />
                 </div>
             ) : null
         }
@@ -129,5 +131,127 @@ const ProductDetails = ({data}) => {
     </div>
   )
 }
+
+const ProductDetailsInfo =({data}) =>{
+    
+    const [active,setActive] = useState(1);
+
+    return(
+        <div className='bg-[#f5f6fb] px-3 800px:px-10 py-2 rounded '>
+            <div className="w-full flex justify-between border-b pt-10 pb-2">
+                <div className='relative'>
+                    <h5 className='text-black text-[#18px] px-1 leading-5 font-[600] cursor-pointer 800px:text-[20px]'
+                    onClick={()=>setActive(1)}
+                    >
+Product Details
+                    </h5>
+                    {
+                        active === 1 ? (
+                            <div className={`${styles.active_indicator}`}/>
+
+                            
+                        ) : null
+                    }
+                </div>
+                <div className='relative'>
+                    <h5 className='text-black text-[#18px] px-1 leading-5 font-[600] cursor-pointer 800px:text-[20px]'
+                    onClick={()=>setActive(2)}
+                    >
+Product Reviews
+                    </h5>
+                    {
+                        active === 2 ? (
+                            <div className={`${styles.active_indicator}`}/>
+
+                            
+                        ) : null
+                    }
+                </div>
+                <div className='relative'>
+                    <h5 className='text-black text-[#18px] px-1 leading-5 font-[600] cursor-pointer 800px:text-[20px]'
+                    onClick={()=>setActive(3)}
+                    >
+Seller Information 
+                    </h5>
+                    {
+                        active === 3 ? (
+                            <div className={`${styles.active_indicator}`}/>
+
+                            
+                        ) : null
+                    }
+                </div>
+            </div>
+            {
+                active === 1 ? (
+<>
+                 <p className='py-2 text-[#18px] leading-8 pb-10 whitespace-pre-line'>
+                    Product details are a curcial part of ant eCommerce website or online marketplace. These details help the ppotentials customers to make an informed desciion about the product . They are interested in buting. A well-written product description can also be a powerful marketing tool that can help to increase sales. Product details typically include information about the products features, specification, dimension,weight, materials, and other relevent information that can help language and be honest and transparent about the products features and linitations 
+                    </p>
+                    <p className='py-2 text-[#18px] leading-8 pb-10 whitespace-pre-line'>
+                   Customer to understand the product better. The product details sectin should also include gigh quality images and videos of the product details it is essential to keep the target audience in mind. The language used should be clear and est to understand. technical terms should be explained in simple language . The tone of profuct dettails shoul be persuasive, highlighting the unique features of the products
+                    </p>
+                    <p className='py-2 text-[#18px] leading-8 pb-10 whitespace-pre-line'>
+                   Customer to understand the product better. The product details sectin should also include gigh quality images and videos of the product details it is essential to keep the target audience in mind. The language used should be clear and est to understand. technical terms should be explained in simple language . The tone of profuct dettails shoul be persuasive, highlighting the unique features of the products
+                    </p>
+                    </>        
+
+                ) : null
+            }
+
+            {
+                active === 2 ? (
+              <div className='w-full justify-center min-h-[40vh] flex items-center'>
+                <p>No Reviews Yet!</p>
+                </div>
+                ) : null
+            }
+
+            {
+                active === 3 && (
+                    <div className="w-full block 800px:flex p-5">
+                        <div className="w-full 800px:w-[50%]">
+                            <div className='flex items-center'>
+                                <img src={data.shop.shop_avatar.url}
+                                alt=''
+                                className='w-[50px] h-[50px] rounded-full'/>
+                                <div className='pl-3'>
+                                    <h3 className={`${styles.shop_name}`}>{data.shop.name}</h3>
+                                    <h5 className='pb-3 text-[15px]'>
+                                        ({data.shop.ratings}) Ratings
+                                    </h5>
+                                    </div>
+                                <p className='pt-2'>
+                                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Mollitia eos sed velit dignissimos ratione nesciunt tenetur, ab neque obcaecati ea illum nisi, ullam cupiditate aspernatur cumque sunt? Nulla, assumenda aliquam.
+                                </p>
+                                </div>
+                        </div>
+
+                        <div className="w-full 800px:w-[50%] mt-5 800px:mt-0 800px:flex flex-col items-end">
+               <div className="text-left">
+                <h5 className="font-[600]">
+Joined on: <span className='font-[500]'>5 FEB,2025</span>
+                </h5>
+                <h5 className='font-[600] pt-3'>
+                    Total Products: <span className='font-[500]'>1,324</span>
+                </h5>
+                <h5 className='font-[600] pt-3'>
+                    Total Reviews: <span className='font-[500]'>124</span>
+                </h5>
+                <Link to='/'>
+                <div className={`${styles.button} !rounded-[4px] !h-[39px] mt-3`}>
+                    <h4 className='text-white'>Visit Shop</h4>
+                </div>
+                </Link>
+               </div>
+                        </div>
+                    </div>
+                ) 
+            }
+                    </div>
+        
+    )
+}
+
 
 export default ProductDetails
