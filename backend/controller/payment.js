6ef92b87a5a4b63539ef51a22eef645e683e349b
Muhammad Subhan <muhammadsubhan8934@gmail.com>
2025-07-15 00:26:04 -0700

    show order reviews ,rating

--- backend/controller/payment.js
+++ backend/controller/payment.js
@@ -1,32 +1,40 @@
 const express = require("express");
 const router = express.Router();
 const catchAsyncError = require("../middleware/catchAsyncError");
+const ErrorHandler = require("../utils/ErrorHandler");
 
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 router.post(
   "/process",
   catchAsyncError(async (req, res, next) => {
-    const myPayment = await stripe.paymentIntents.create({
-      amount: req.body.amount,
-      currency: "pkr",
-      metadata: {
-        company: "Muhammad Subhan",
-      },
-    });
-    res.status(200).json({
-      success: true,
-      client_secret: myPayment.client_secret,
-    });
+    try {
+      const myPayment = await stripe.paymentIntents.create({
+        amount: req.body.amount,
+        currency: "pkr",
+        metadata: {
+          company: "Muhammad Subhan",
+        },
+      });
+      res.status(200).json({
+        success: true,
+        client_secret: myPayment.client_secret,
+      });
+    } catch (error) {
+      return next(new ErrorHandler(error.message, 500));
+    }
   })
 );
 
 router.get(
   "/stripeapikey",
   catchAsyncError(async (req, res, next) => {
-    res.status(200).json({ stripeApikey: process.env.STRIPE_API_KEY });
+    try {
+      res.status(200).json({ stripeApikey: process.env.STRIPE_API_KEY });
+    } catch (error) {
+      return next(new ErrorHandler(error.message, 500));
+    }
   })
 );
-
 
 module.exports = router;
--- backend/controller/product.js
+++ backend/controller/product.js
@@ -7,6 +7,8 @@ const { upload } = require("../multer")
 const Product = require("../model/product");
 const {isSeller} = require("../middleware/auth")
 const fs = require('fs');
+const Order = require("../model/order")
+const {isAuthentication} = require("../middleware/auth")
 
 // create product
 router.post("/create-product", upload.array("images"), catchAsyncError(async (req, res, next) => {
@@ -148,5 +150,82 @@ router.delete("/delete-shop-product/:id", isSeller, catchAsyncError(async (req, 
         return next(new ErrorHandler(error, 400));
     }
 }));
+
+// review for a product
+router.put(
+  "/create-new-review",
+  isAuthentication,
+  catchAsyncError(async (req, res, next) => {
+    try {
+      const { user, rating, comment, productId, orderId } = req.body;
+
+      const product = await Product.findById(productId);
+
+      const review = {
+        user,
+        rating,
+        comment,
+        productId,
+      };
+
+      const isReviewed = product.reviews.find(
+        (rev) => rev.user._id === req.user._id
+      );
+
+      if (isReviewed) {
+        product.reviews.forEach((rev) => {
+          if (rev.user._id === req.user._id) {
+            (rev.rating = rating), (rev.comment = comment), (rev.user = user);
+          }
+        });
+      } else {
+        product.reviews.push(review);
+      }
+
+      let avg = 0;
+
+      product.reviews.forEach((rev) => {
+        avg += rev.rating;
+      });
+
+      product.ratings = avg / product.reviews.length;
+
+      await product.save({ validateBeforeSave: false });
+
+      await Order.findByIdAndUpdate(
+        orderId,
+        { $set: { "cart.$[elem].isReviewed": true } },
+        { arrayFilters: [{ "elem._id": productId }], new: true }
+      );
+
+      res.status(200).json({
+        success: true,
+        message: "Reviwed succesfully!",
+      });
+    } catch (error) {
+      return next(new ErrorHandler(error, 400));
+    }
+  })
+);
+
+// all products --- for admin
+// router.get(
+//   "/admin-all-products",
+//   isAuthenticated,
+//   isAdmin("Admin"),
+//   catchAsyncErrors(async (req, res, next) => {
+//     try {
+//       const products = await Product.find().sort({
+//         createdAt: -1,
+//       });
+//       res.status(201).json({
+//         success: true,
+//         products,
+//       });
+//     } catch (error) {
+//       return next(new ErrorHandler(error.message, 500));
+//     }
+//   })
+// );
 
 module.exports = router;
